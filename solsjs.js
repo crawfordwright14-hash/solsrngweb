845a6797904cb7118a5afb8ad698a66697ad2e98
crawfordwright14-hash <crawfordwright14@gmail.com>
2026-02-28 18:57:10 -0500

    crazy global event
    ergergergerg

--- solsjs.js
+++ solsjs.js
@@ -1,5 +1,5 @@
 let should = false
-let multi = 10000000
+let multi = 100000000
 const luckb = document.getElementById("lg")
 const crys = document.getElementById("cd")
 const topa = document.getElementById("tg")
